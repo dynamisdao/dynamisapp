cc75889cc46b9fe6a3518c951dcde51291dd8a6f
Aleksey <chepe4pi@gmail.com>
2016-08-31 16:05:32 +0000

    cover all account serializers by tests

---
+++ tests/python/accounts/test_account_serializers.py
@@ -0,0 +1,65 @@
+import datetime
+
+from dynamis.apps.accounts.api.v1.serializers import AccountConfigSerializer, AccountShortSerializer, \
+    AccountDetailSerializer, AccountListSerializer
+
+
+def test_account_settings_serializer(factories):
+    account_settings = factories.AccountConfigFactory(rpc_node_host='http://example.com/test_url')
+
+    data = {
+        'rpc_node_host': account_settings.rpc_node_host,
+    }
+
+    serializer = AccountConfigSerializer(account_settings)
+    assert serializer.data == data
+
+
+def test_account_short_serializer(factories):
+    account = factories.UserFactory()
+
+    data = {
+        'keybase_username': account.keybase_username,
+        'keybase_verified': account.is_keybase_verified,
+        'email': account.email
+
+    }
+
+    serializer = AccountShortSerializer(account)
+    assert serializer.data == data
+
+
+def test_account_detail_serializer(factories):
+    date_now = datetime.datetime.utcnow()
+    account = factories.UserFactory(date_joined=date_now)
+
+    data = {
+        'keybase_username': account.keybase_username,
+        'keybase_verified': account.is_keybase_verified,
+        'email': account.email,
+        'date_joined': date_now.isoformat(),
+        'last_login': account.last_login,
+        'verified_at': account.verified_at,
+        'superuser': account.is_superuser,
+        'staff': account.is_staff,
+        'active': account.is_active,
+        'risk_assessor': account.is_risk_assessor,
+        'email_verified': False,
+        'id': account.id,
+    }
+
+    serializer = AccountDetailSerializer(account)
+    assert serializer.data == data
+
+
+def test_account_list_serializer(factories):
+    account = factories.UserFactory()
+
+    data = {
+        'keybase_username': account.keybase_username,
+        'email': account.email,
+        'id': account.id,
+    }
+
+    serializer = AccountListSerializer(account)
+    assert serializer.data == data
