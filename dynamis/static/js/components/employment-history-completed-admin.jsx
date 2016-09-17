c5b5221b825f3d5922e0d73951532369e70831ac
Aleksey <chepe4pi@gmail.com>
2016-09-17 22:16:13 +0300

    fix missing variable

--- dynamis/static/js/components/employment-history-completed-admin.jsx
+++ dynamis/static/js/components/employment-history-completed-admin.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import {connectRedux} from '../utils';
 
 export default connectRedux(React.createClass({
-  render() {
-    return (
-      <div className="card">
-        <ul className="collection">
-          <li className="collection-item">From: {monthNames[this.props.jobData.startMonth]} {this.props.jobData.startYear}</li>
-          <li className="collection-item">To: {monthNames[this.props.jobData.endMonth]} {this.props.jobData.endYear}</li>
-          <li className="collection-item">Notes: {this.props.jobData.notes}</li>
-          <li className="collection-item">Company: {this.props.jobData.company}</li>
-        </ul>
-      </div>
-    );
-  },
+    render() {
+        var monthNames = [
+            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+        ];
+        return (
+            <div className="card">
+                <ul className="collection">
+                    <li className="collection-item">
+                        From: {monthNames[this.props.jobData.startMonth]} {this.props.jobData.startYear}</li>
+                    <li className="collection-item">
+                        To: {monthNames[this.props.jobData.endMonth]} {this.props.jobData.endYear}</li>
+                    <li className="collection-item">Notes: {this.props.jobData.notes}</li>
+                    <li className="collection-item">Company: {this.props.jobData.company}</li>
+                </ul>
+            </div>
+        );
+    },
 }));
