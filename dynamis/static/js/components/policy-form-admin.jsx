d7508c6c0421474f317a5ba68794f4abcc79e999
Aleksey <chepe4pi@gmail.com>
2016-09-18 11:21:54 +0300

    add status and email to admin policy view

--- dynamis/static/js/components/policy-form-admin.jsx
+++ dynamis/static/js/components/policy-form-admin.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
 import {connectRedux} from '../utils';
-import IdentityFormSection from './identity-form-section';
 import {AnimateFade} from './animator';
 import EmploymentHistoryFormSectionAdmin from './employment-history-form-section-admin';
-import PolicyFormControls from './policy-form-controls';
 
 export default connectRedux(React.createClass({
-  render() {
-    return (
-      <div style={{marginRight: '50px', marginLeft: '50px'}}>
-        <h1>Policy Application: {this.props.store.policy.id}</h1>
-        <IdentityFormSection />
-        <AnimateFade>
-          {this.renderEmploymentHistorySectionAdmin()}
-        </AnimateFade>
-      </div>
-    );
-  },
-  renderEmploymentHistorySectionAdmin() {
-    if(this.props.store.account.authenticated) {
-      return (
-        <div>
-          <div className="divider"></div>
-          <EmploymentHistoryFormSectionAdmin />
-        </div>
-      );
-    }
-    return null;
-  },
+    render() {
+        return (
+            <div style={{marginRight: '50px', marginLeft: '50px'}}>
+                <h1>Policy Application: {this.props.store.policy.id}</h1>
+                <div>
+                    <h1>Identity: {this.props.store.policy.user.email}</h1>
+                    <AnimateFade>
+                        <h3>{this.props.store.policy.user.email}</h3>
+                    </AnimateFade>
+                </div>
+                <AnimateFade>
+                    Status: {this.renderPolicyStatusAdmin()}
+                </AnimateFade>
+                <AnimateFade>
+                    {this.renderEmploymentHistorySectionAdmin()}
+                </AnimateFade>
+            </div>
+        );
+    },
+    renderEmploymentHistorySectionAdmin() {
+        if (this.props.store.account.authenticated) {
+            return (
+                <div>
+                    <div className="divider"></div>
+                    <EmploymentHistoryFormSectionAdmin />
+                </div>
+            );
+        }
+        return null;
+    },
+
+    renderPolicyStatusAdmin() {
+        if (this.props.store.policy.is_final) {
+            return (
+                <span class="badge green">Under Review</span>
+            );
+        }
+        else {
+            return <span class="badge green">Incomplete</span>
+        }
+    },
 }));
