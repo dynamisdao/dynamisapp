f6f3b7e1c49acbd1fd9576fc97dc19cbeba63fe6
Aleksey <chepe4pi@gmail.com>
2016-09-20 13:02:08 +0300

    TASK-48: Assign Risk Assessor tasks to random users-assessors when p2p review is done. Cover all cases by unit-tests

---
+++ tests/python/policies/test_policy_states_side_effects.py
@@ -0,0 +1,131 @@
+from dynamis.apps.policy.models import POLICY_STATUS_INIT, POLICY_STATUS_SUBMITTED, \
+    POLICY_STATUS_ON_SMART_DEPOSIT_REFUND, POLICY_STATUS_ON_P2P_REVIEW, POLICY_STATUS_ON_RISK_ASSESSMENT_REVIEW, \
+    POLICY_STATUS_APPROVED, POLICY_STATUS_ACTIVE, POLICY_STATUS_WAIT_FOR_PREMIUM, RiskAssessmentTask
+from dynamis.settings import RISK_ASSESSORS_PER_POLICY_COUNT
+
+
+def test_to_deposit_refund(factories):
+    user = factories.UserFactory()
+    policy = factories.PolicyApplicationFactory(user=user)
+    deposit = factories.SmartDepositFactory(user=user, is_confirmed=True)
+    policy.to_deposit_refund()
+    assert policy.rejected_count == 1
+
+
+def test_p2p_review_to_risk_assessment_review_lack_of_assessors(factories):
+    user = factories.UserFactory()
+
+    user_2 = factories.UserFactory(is_risk_assessor=True)
+    user_3 = factories.UserFactory(is_risk_assessor=True)
+    user_4 = factories.UserFactory(is_risk_assessor=True)
+
+    deposit = factories.SmartDepositFactory(user=user, is_confirmed=True)
+    policy = factories.PolicyApplicationFactory(state=POLICY_STATUS_ON_P2P_REVIEW,
+                                                user=user)
+    app_item = factories.IdentityApplicationItemFactory(policy_application=policy)
+    factories.IdentityPeerReviewFactory(application_item=app_item, result='3')
+
+    policy.p2p_review_to_risk_assessment_review()
+    assert policy.state == POLICY_STATUS_ON_RISK_ASSESSMENT_REVIEW
+
+    assert RiskAssessmentTask.objects.all().count() == 3
+
+
+def test_p2p_review_to_risk_assessment_review_user_is_assessor(factories):
+    user = factories.UserFactory(is_risk_assessor=True)
+    user_2 = factories.UserFactory(is_risk_assessor=True)
+    user_3 = factories.UserFactory(is_risk_assessor=True)
+    deposit = factories.SmartDepositFactory(user=user, is_confirmed=True)
+    policy = factories.PolicyApplicationFactory(state=POLICY_STATUS_ON_P2P_REVIEW,
+                                                user=user)
+    app_item = factories.IdentityApplicationItemFactory(policy_application=policy)
+    factories.IdentityPeerReviewFactory(application_item=app_item, result='3')
+
+    policy.p2p_review_to_risk_assessment_review()
+    assert policy.state == POLICY_STATUS_ON_RISK_ASSESSMENT_REVIEW
+
+    assert RiskAssessmentTask.objects.all().count() == 2
+    assert RiskAssessmentTask.objects.filter(user=user).exists() is False
+
+
+def test_p2p_review_to_risk_assessment_review_enough_assessors(factories):
+    user = factories.UserFactory()
+
+    user_2 = factories.UserFactory(is_risk_assessor=True)
+    user_3 = factories.UserFactory(is_risk_assessor=True)
+    user_4 = factories.UserFactory(is_risk_assessor=True)
+
+    user_5 = factories.UserFactory(is_risk_assessor=True)
+    user_6 = factories.UserFactory(is_risk_assessor=True)
+    user_7 = factories.UserFactory(is_risk_assessor=True)
+
+    deposit = factories.SmartDepositFactory(user=user, is_confirmed=True)
+    policy = factories.PolicyApplicationFactory(state=POLICY_STATUS_ON_P2P_REVIEW,
+                                                user=user)
+    app_item = factories.IdentityApplicationItemFactory(policy_application=policy)
+    factories.IdentityPeerReviewFactory(application_item=app_item, result='3')
+
+    policy.p2p_review_to_risk_assessment_review()
+    assert policy.state == POLICY_STATUS_ON_RISK_ASSESSMENT_REVIEW
+
+    assert RiskAssessmentTask.objects.all().count() == RISK_ASSESSORS_PER_POLICY_COUNT
+
+
+def test_p2p_review_to_risk_assessment_review_random_assessors(factories):
+    user = factories.UserFactory()
+    user_0 = factories.UserFactory()
+
+    user_2 = factories.UserFactory(is_risk_assessor=True)
+    user_3 = factories.UserFactory(is_risk_assessor=True)
+    user_4 = factories.UserFactory(is_risk_assessor=True)
+
+    user_5 = factories.UserFactory(is_risk_assessor=True)
+    user_6 = factories.UserFactory(is_risk_assessor=True)
+    user_7 = factories.UserFactory(is_risk_assessor=True)
+
+    user_8 = factories.UserFactory(is_risk_assessor=True)
+    user_9 = factories.UserFactory(is_risk_assessor=True)
+    user_10 = factories.UserFactory(is_risk_assessor=True)
+
+    deposit = factories.SmartDepositFactory(user=user, is_confirmed=True)
+    policy = factories.PolicyApplicationFactory(state=POLICY_STATUS_ON_P2P_REVIEW,
+                                                user=user)
+    app_item = factories.IdentityApplicationItemFactory(policy_application=policy)
+    factories.IdentityPeerReviewFactory(application_item=app_item, result='3')
+
+    deposit_2 = factories.SmartDepositFactory(user=user_0, is_confirmed=True)
+    policy_2 = factories.PolicyApplicationFactory(state=POLICY_STATUS_ON_P2P_REVIEW,
+                                                  user=user_0)
+    app_item_2 = factories.IdentityApplicationItemFactory(policy_application=policy_2)
+    factories.IdentityPeerReviewFactory(application_item=app_item_2, result='3')
+
+    policy.p2p_review_to_risk_assessment_review()
+    assert policy.state == POLICY_STATUS_ON_RISK_ASSESSMENT_REVIEW
+
+    policy_2.p2p_review_to_risk_assessment_review()
+    assert policy.state == POLICY_STATUS_ON_RISK_ASSESSMENT_REVIEW
+
+    assert RiskAssessmentTask.objects.all().count() == RISK_ASSESSORS_PER_POLICY_COUNT * 2
+
+    first_assessors = RiskAssessmentTask.objects.filter(policy=policy).values_list('user__id', flat=True)
+    second_assessors = RiskAssessmentTask.objects.filter(policy=policy_2).values_list('user__id', flat=True)
+
+    assert sorted(first_assessors) != sorted(second_assessors)
+
+
+def test_p2p_review_to_risk_assessment_already_created(factories):
+    user = factories.UserFactory()
+    user_assessor = factories.UserFactory()
+    deposit = factories.SmartDepositFactory(user=user, is_confirmed=True)
+    policy = factories.PolicyApplicationFactory(state=POLICY_STATUS_ON_P2P_REVIEW,
+                                                user=user)
+    app_item = factories.IdentityApplicationItemFactory(policy_application=policy)
+    factories.IdentityPeerReviewFactory(application_item=app_item, result='3')
+
+    factories.RiskAssessmentTaskFactory.create_batch(5, policy=policy)
+    assert RiskAssessmentTask.objects.all().count() == 5
+
+    policy.p2p_review_to_risk_assessment_review()
+    assert policy.state == POLICY_STATUS_ON_RISK_ASSESSMENT_REVIEW
+
+    assert RiskAssessmentTask.objects.all().count() == 5
