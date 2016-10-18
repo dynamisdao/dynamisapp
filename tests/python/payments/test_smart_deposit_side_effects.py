eaf62d179b40997d96db19f5f20f007ce64ee666
Alexey Kurylev <chepe4pi@gmail.com>
2016-10-18 18:15:41 +0300

    TASK-169: Get ETH price from coinmarketcap

--- tests/python/payments/test_smart_deposit_side_effects.py
+++ tests/python/payments/test_smart_deposit_side_effects.py
@@ -1,10 +1,10 @@
 from constance import config
 from django.utils import timezone
 
-from dynamis.apps.payments.models import SmartDeposit
+from dynamis.apps.payments.models import SmartDeposit, SMART_DEPOSIT_STATUS_WAITING
 
 
-def test_check_wait_for_deposit_time(factories):
+def test_check_wait_for_deposit_time(factories, mock_request_exchange_rate):
     user = factories.UserFactory()
     policy = factories.PolicyApplicationFactory(user=user)
     deposit = factories.SmartDepositFactory(policy=policy, state=0)
@@ -14,3 +14,13 @@ def test_check_wait_for_deposit_time(factories):
 
     assert deposit.wait_for > timezone.now()
     assert deposit.exchange_rate_at_invoice_time == config.DOLLAR_ETH_EXCHANGE_RATE
+    assert config.DOLLAR_ETH_EXCHANGE_RATE == 12.686
+
+
+def test_check_wait_to_init(factories, mock_request_exchange_rate):
+    user = factories.UserFactory()
+    policy = factories.PolicyApplicationFactory(user=user)
+    deposit = factories.SmartDepositFactory(policy=policy, state=SMART_DEPOSIT_STATUS_WAITING)
+    deposit.wait_to_init()
+
+    assert config.DOLLAR_ETH_EXCHANGE_RATE == 12.686
